4ad0dae3c066e4c8695b18c56dd0453e647bbd42
Hugo German Cuellar Martinez <comandorb_132@yahoo.com.mx>
2018-11-15 18:16:47 -0600

    ya jalo

--- src/index.php
+++ src/index.php
@@ -1,2 +1,15 @@
 <?php
-echo "hola mundo";
+$servername = "192.168.33.22";
+$username = "root";
+$password = "12345678";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=curso", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
